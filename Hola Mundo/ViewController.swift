aa85fe2f34c3312d209b1908bd8b5de2eec9b5b3
Carlos Palacios <capcor88@imac-de-carlos.local>
2016-07-21 23:14:42 -0500

    se ha modificado el programa para que realice funciónes basicas

--- Hola Mundo/ViewController.swift
+++ Hola Mundo/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var texto: UITextField!
+    @IBOutlet weak var mensaje: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cambiar(sender: AnyObject) {
+        mensaje.text = "hola \(texto.text!)"
+        view.endEditing(true)
+        
+        //comentarios
+    }
 
 }
 
